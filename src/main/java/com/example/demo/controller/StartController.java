b6330deaee44ff1e5b7218bc069ffa3561fbe8f1
newyouth1 <42432832+newyouth1@users.noreply.github.com>
2018-10-27 20:38:34 +0800

    github修改！
      System.out.println("github修改！");

--- src/main/java/com/example/demo/controller/StartController.java
+++ src/main/java/com/example/demo/controller/StartController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StartController {
     @RequestMapping("/springboot")
     public String startSpringBoot() {
-        System.out.println("本地修改！");
+        System.out.println("github修改！");
         return "Welcome to the world of Spring Boot! 哈哈哈哈冲突测试！";
     }
 }
